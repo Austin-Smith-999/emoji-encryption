d985059455016f5e3c2d594c88a5f3822f5ed29c
Austin Smith <austin.smith.ads@gmail.com>
2022-04-20 21:46:35 -0500

    added initial index.js code with throw error log

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = function EmojiEncryption(string) {
+    if (typeof string !== "string") throw new TypeError(
+    "Only letters A-Z allowed and the following symbols allowed: `~!@#$%^&*()_+-=[{]}\|;:\'\"\,\<\>\./?");
+    
+  };
+
